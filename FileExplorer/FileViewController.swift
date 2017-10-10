dd7ecbd389dda7b3dee3b612d8fc989b7dc9a189
henry Phan <henry@unima.vn>
2017-10-10 10:47:46 +0700

    update

--- FileExplorer/FileViewController.swift
+++ FileExplorer/FileViewController.swift
@@ -151,7 +151,7 @@ final class TitleView: UIView {
         titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -1.0).isActive = true
         titleLabel.widthAnchor.constraint(equalTo: widthAnchor, constant: -20.0).isActive = true
-        titleLabel.setContentHuggingPriority(UILayoutPriority(rawValue: UILayoutPriority.required.rawValue), for: .vertical)
+        titleLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 1000), for: .vertical)
         titleLabel.numberOfLines = 1
         titleLabel.lineBreakMode = .byTruncatingTail
 
